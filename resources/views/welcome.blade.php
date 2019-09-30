d67e4e04618a59f9267f681196c674e0805ee493
DDoeDDoe32 <ddoeddoe32@naver.com>
2019-09-30 23:25:03 +0900

    블레이드

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,18 @@
-<h1><?= isset($greeting) ? "{$greeting} " : 'Hello '; ?><?=$name; ?></h1>
+@extends('layouts.master')
+
+@section('style')
+    <style>
+        body {background: green; color:white;}
+    </style>
+@endsection
+
+@section('content')
+    <p>저는 자식 뷰의 'content' 섹션입니다.</p>
+    @include('partials.footer')
+@endsection
+
+@section('script')
+    <script>
+        alert("저는 자식 뷰의 'script' 섹션입니다.");
+    </script>
+@endsection
